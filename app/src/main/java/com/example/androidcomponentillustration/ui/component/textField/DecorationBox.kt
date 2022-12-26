63473ef2a7e7666a268c165bdaf061de350e7844
Raheem Jnr <mumedian6@gmail.com>
2022-12-26 13:35:18 +0100

    decoration box

--- app/src/main/java/com/example/androidcomponentillustration/ui/component/textField/DecorationBox.kt
+++ app/src/main/java/com/example/androidcomponentillustration/ui/component/textField/DecorationBox.kt
@@ -1,36 +1,60 @@
 package com.example.androidcomponentillustration.ui.component.textField
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.rounded.Person
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun TextFields() {
     Scaffold(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
     ) { contentPadding ->
         Column(
-            Modifier.padding(contentPadding)
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(contentPadding)
         ) {
             var input by remember { mutableStateOf("") }
 
-            Column(horizontalAlignment = Alignment.CenterHorizontally,
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center,
                 modifier = Modifier
-                    .padding(vertical = 12.dp)
             ) {
                 BasicTextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .border(width = 1.5.dp, color = Color.Black)
+                        .padding(12.dp),
                     value = input,
                     onValueChange = {
                         input = it
+                    },
+                    decorationBox = { innerTextField ->
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Text(text = "Hi enter your text")
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Icon(Icons.Rounded.Person, contentDescription = "")
+                                innerTextField()
+                                Icon(Icons.Rounded.Check, contentDescription = "")
+
+                            }
+                        }
+
                     }
                 )
             }
