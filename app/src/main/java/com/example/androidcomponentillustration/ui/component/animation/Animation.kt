c6b8b430dc16b791bdfc320235a3f965cc249e98
Raheem Jnr <mumedian6@gmail.com>
2023-01-06 19:41:35 +0100

    decoration box

--- app/src/main/java/com/example/androidcomponentillustration/ui/component/animation/Animation.kt
+++ app/src/main/java/com/example/androidcomponentillustration/ui/component/animation/Animation.kt
@@ -1,15 +1,18 @@
 package com.example.androidcomponentillustration.ui.component.animation
 
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.background
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -29,17 +32,44 @@ fun Animation() {
                 verticalArrangement = Arrangement.Center,
                 modifier = Modifier
             ) {
-                val infiniteTransition = rememberInfiniteTransition()
-                val color by infiniteTransition.animateColor(
-                    initialValue = Color.Red,
-                    targetValue = Color.Green,
-                    animationSpec = infiniteRepeatable(
-                        animation = tween(1000, easing = LinearEasing),
-                        repeatMode = RepeatMode.Reverse
+//                val infiniteTransition = rememberInfiniteTransition()
+//                val color by infiniteTransition.(
+//                    initialValue = 0f,
+//                    targetValue = 1f,
+//                    animationSpec = infiniteRepeatable(
+//                        animation = spring(),
+//                        repeatMode = RepeatMode.Reverse
+//                    )
+//                )
+                val value by animateFloatAsState(
+                    targetValue = 1f,
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioHighBouncy,
+                        stiffness = Spring.StiffnessMedium
                     )
                 )
 
-                Box(Modifier.fillMaxSize().background(color))
+
+//                animateColor(
+//                    initialValue = Color.Red,
+//                    targetValue = Color.Green,
+//                    animationSpec = infiniteRepeatable(
+//                        animation = tween(1000, easing = LinearEasing),
+//                        repeatMode = RepeatMode.Reverse
+//                    )
+//                )
+
+                Box(Modifier.fillMaxSize()) {
+
+                    Icon(
+                        Icons.Default.Favorite,
+                        contentDescription = "",
+                        modifier = Modifier.size(150.dp)
+                            .offset(
+                            y = value.dp
+                        )
+                    )
+                }
             }
         }
 
